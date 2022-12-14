f9fa412e3d65cdcc383428a1da9285c85d464a70
mohammed balegh <38575688+mohammedbalegh@users.noreply.github.com>
2022-12-14 20:43:59 +0200

    first commit

---
+++ Thirdwayv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thirdwayv
+//
+//  Created by mohammed balegh on 14/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
